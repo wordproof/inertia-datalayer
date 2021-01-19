7969d30965bb4917436528d93d68453059028682
YM <yurii@wordproof.com>
2021-01-19 08:56:19 +0200

    Logic update

--- src/dataLayer.js
+++ src/dataLayer.js
@@ -1,25 +1,42 @@
-function storageData(modelObject) {
+function storageData() {
 
-    let storageData = [];
+    let $this = this;
+
+    $this.storageData = [];
+    $this.allowedKeys = [];
 
     return {
 
-        find: function (value) {
-            return value ? storageData.find(value) : storageData;
+        init: function (allowedKeys) {
+            $this.allowedKeys = allowedKeys;
         },
 
-        push: function (element) {
-            storageData.push(element);
+        find: function (value) {
+            return value ? $this.storageData.find(value) : $this.storageData;
+        },
+
+        set: function (value, key) {
+
+            if (typeof key !== 'undefined') {
+                if (typeof $this.allowedKeys.find(el => el === key) === 'undefined')
+                    return false;
+                else
+                    $this.storageData[key] = value;
+            }
+
+            if (typeof value === 'object') {
+                for (const [key, val] of Object.entries(value)) {
+                    if (typeof $this.allowedKeys.find(el => el === key) !== 'undefined')
+                        $this.storageData[key] = val;
+                }
+            }
+
+            return true;
         },
 
         del: function (key) {
-            if (typeof storageData[key] !== 'undefined')
-                storageData[key].delete();
-        },
-
-        set: function (key, value) {
-            if (typeof storageData[key] !== 'undefined')
-                storageData[key] = value;
+            if (typeof $this.storageData[key] !== 'undefined')
+                $this.storageData[key].delete();
         }
 
     }
